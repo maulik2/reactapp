e45df192bf945ebb03dff91ae5bf81cd1f85c760
maulik2 <maulik2@umbc.edu>
2018-02-10 13:30:26 -0500

    added component tests

--- src/App.js
+++ src/App.js
@@ -1,19 +1,59 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import localimage from './me.png';
 
 class App extends Component {
   render() {
+      const now = new Date();
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p> App Component Test </p>
+        <h1> The date is: { now.toTimeString() } </h1>
+        <hr></hr>
+        <Biography/>
+        <hr></hr>
+        <GroceryList/>
+
       </div>
+    );
+  }
+}
+
+class Biography extends React.Component {
+  render(){
+    return (
+        <div>
+          <p> Biography Component Test </p>
+          <p> Local image </p>
+          <img src={localimage}/>
+            <p> Web image </p>
+          <img src="http://www.rednewswire.com/wp-content/uploads/2016/08/apple.jpg" style={{width:100, lenght:100}}/>
+          <p> Name: Maulik Zalavadia </p>
+          <p> Skill: Developing </p>
+        </div>
+    );
+  }
+}
+
+class GroceryList extends React.Component {
+  render(){
+       const list = ['Banana','Cookies','Ice Cream',"Strawberry"]  
+       const numlist = ['1','2','3',"4"]  
+    
+    return (
+        <div>
+          <p> Grocery list Component Test </p>
+         
+          <p>
+           <h1>
+              {list.map(list => <li>{list}</li>)}
+            </h1>
+            <ul>
+             {numlist.map(list => <li>{list}</li>)}
+            </ul>
+         </p>
+        </div>
     );
   }
 }
